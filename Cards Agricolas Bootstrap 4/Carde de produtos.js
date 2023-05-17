170a50d144fba74a87c3ad4c28355178acf77170
ASPPIBRA-DEV <sandro_ceo@asppibra.com.br>
2023-05-17 09:31:22 -0300

    Update Carde de produtos.js

--- Cards Agricolas Bootstrap 4/Carde de produtos.js
+++ Cards Agricolas Bootstrap 4/Carde de produtos.js
@@ -27,3 +27,18 @@ var card = document.querySelector('.card');
 	card6.addEventListener( 'click', function() {
 	  card6.classList.toggle('is-flipped');
 	});
+
+var card7 = document.querySelector('.card7');
+	card7.addEventListener( 'click', function() {
+	  card7.classList.toggle('is-flipped');
+	});
+	
+	var card8 = document.querySelector('.card8');
+	card8.addEventListener( 'click', function() {
+	  card8.classList.toggle('is-flipped');
+	});
+	
+	var card9 = document.querySelector('.card9');
+	card9.addEventListener( 'click', function() {
+	  card9.classList.toggle('is-flipped');
+	});
